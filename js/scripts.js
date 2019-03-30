7618b1bd1d80721bff315fd86822573e2456d782
Lindsey Baker <lindsey.baker0@gmail.com>
2019-03-29 18:36:02 -0700

    Add function on #questionReveal

--- js/scripts.js
+++ js/scripts.js
@@ -14,45 +14,51 @@ $(document).ready(function(){
 // If front end return .java, if not ask further questions
 
     if (questionOne === "frontEnd"){
+      $(".type").hide();
       $(".design").show();
-      $(".idk").hide();
       $("#questionReveal").hide();
     } else if (questionOne === "backEnd"){
       $(".type").hide();
       $("#questionReveal").show();
     } else if (questionOne === "idk"){
+      $(".type").hide();
       $(".idk").show();
-      $(".design").hide();
       $("#questionReveal").hide();
 
 // If user selects similar results in 2 or more responses in questionTwo, questionThree or questionFour return the repested results
 
-    } if ((questionOne === "backEnd") && ((result === "prr") || (result === "rpr") || (result === "rrp") || (result === "crr") || (result === "rcr") || (result === "rrc") || (result === "rjr") || (result === "rrr"))) {
-      $(".type").hide();
-      $(".ruby").show();
-    } else if ((questionOne === "backEnd") && ((result === "cjp") || (result === "cjr") || (result === "cjc"))) {
-      $(".type").hide();
-      $(".java").show();
-    } else if ((questionOne === "backEnd") && ((result === "ccp") || (result === "cpc") || (result === "pcc") || (result === "ccr") || (result === "rcc") || (result === "crc") || (result === "cjc") || (result === "ccc"))) {
-      $(".type").hide();
-      $(".c").show();
-    } else if ((questionOne === "backEnd") && ((result === "jpp") || (result === "pjp") || (result === "ppc") || (result === "cpp") || (result === "pcp") || (result === "ppr") || (result === "rpp") || (result === "prp") || (result === "rpp") || (result === "ppp")) ) {
-      $(".type").hide();
-      $(".php").show();
-
-// If user does not select similar results in 2 or more responses in questionTwo, questionThree or questionFour use value from questionFive
-
-    } else if (!((result === "prr") || (result === "rpr") || (result === "rrp") || (result === "crr") || (result === "rcr") || (result === "rrc") || (result === "rjr") || (result === "rrr") || (result === "cjp") || (result === "cjr") || (result === "cjc") || (result === "jpp") || (result === "pjp") || (result === "ppc") || (result === "cpp") || (result === "pcp") || (result === "ppr") || (result === "rpp") || (result === "prp") || (result === "rpp") || (result === "ppp")) && questionFive === "p") {
-      $(".type").hide();
-      $(".php").show();
-    } else if (!((result === "prr") || (result === "rpr") || (result === "rrp") || (result === "crr") || (result === "rcr") || (result === "rrc") || (result === "rjr") || (result === "rrr") || (result === "cjp") || (result === "cjr") || (result === "cjc") || (result === "jpp") || (result === "pjp") || (result === "ppc") || (result === "cpp") || (result === "pcp") || (result === "ppr") || (result === "rpp") || (result === "prp") || (result === "rpp") || (result === "ppp")) && questionFive === "j") {
-      $(".type").hide();
-      $(".java").show();
-    } else if (!((result === "prr") || (result === "rpr") || (result === "rrp") || (result === "crr") || (result === "rcr") || (result === "rrc") || (result === "rjr") || (result === "rrr") || (result === "cjp") || (result === "cjr") || (result === "cjc") || (result === "jpp") || (result === "pjp") || (result === "ppc") || (result === "cpp") || (result === "pcp") || (result === "ppr") || (result === "rpp") || (result === "prp") || (result === "rpp") || (result === "ppp")) && questionFive === "r") {
-      $(".type").hide();
-      $(".ruby").show();
     }
-});
+    $("#questionReveal").submit(function(event){
+      if ((questionOne === "backEnd") && ((result === "prr") || (result === "rpr") || (result === "rrp") || (result === "crr") || (result === "rcr") || (result === "rrc") || (result === "rjr") || (result === "rrr"))) {
+        $(".type").hide();
+        $(".ruby").show();
+      } else if ((questionOne === "backEnd") && ((result === "cjp") || (result === "cjr") || (result === "cjc"))) {
+        $(".type").hide();
+        $(".java").show();
+      } else if ((questionOne === "backEnd") && ((result === "ccp") || (result === "cpc") || (result === "pcc") || (result === "ccr") || (result === "rcc") || (result === "crc") || (result === "cjc") || (result === "ccc"))) {
+        $(".type").hide();
+        $(".c").show();
+      } else if ((questionOne === "backEnd") && ((result === "jpp") || (result === "pjp") || (result === "ppc") || (result === "cpp") || (result === "pcp") || (result === "ppr") || (result === "rpp") || (result === "prp") || (result === "rpp") || (result === "ppp")) ) {
+        $(".type").hide();
+        $(".php").show();
 
+  // If user does not select similar results in 2 or more responses in questionTwo, questionThree or questionFour use value from questionFive
 
+      } else if (!((result === "prr") || (result === "rpr") || (result === "rrp") || (result === "crr") || (result === "rcr") || (result === "rrc") || (result === "rjr") || (result === "rrr") || (result === "cjp") || (result === "cjr") || (result === "cjc") || (result === "jpp") || (result === "pjp") || (result === "ppc") || (result === "cpp") || (result === "pcp") || (result === "ppr") || (result === "rpp") || (result === "prp") || (result === "rpp") || (result === "ppp")) && questionFive === "p") {
+        $(".type").hide();
+        $(".php").show();
+      } else if (!((result === "prr") || (result === "rpr") || (result === "rrp") || (result === "crr") || (result === "rcr") || (result === "rrc") || (result === "rjr") || (result === "rrr") || (result === "cjp") || (result === "cjr") || (result === "cjc") || (result === "jpp") || (result === "pjp") || (result === "ppc") || (result === "cpp") || (result === "pcp") || (result === "ppr") || (result === "rpp") || (result === "prp") || (result === "rpp") || (result === "ppp")) && questionFive === "j") {
+        $(".type").hide();
+        $(".java").show();
+      } else if (!((result === "prr") || (result === "rpr") || (result === "rrp") || (result === "crr") || (result === "rcr") || (result === "rrc") || (result === "rjr") || (result === "rrr") || (result === "cjp") || (result === "cjr") || (result === "cjc") || (result === "jpp") || (result === "pjp") || (result === "ppc") || (result === "cpp") || (result === "pcp") || (result === "ppr") || (result === "rpp") || (result === "prp") || (result === "rpp") || (result === "ppp")) && questionFive === "r") {
+        $(".type").hide();
+        $(".ruby").show();
+      }
+
+      event.preventDefault();
+
+    });
+
+  });
+  
 });
